90658481c2ca2513c9bfb56d28cba94c750bc0af
Daniel Boley <danboley1@gmail.com>
2022-08-30 15:20:32 -0400

    commit tree 9f7875fb403b05dc08dfe977425930ef28e12b2a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    return cats.push(Ralph);
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    let newCats = [...cats, "Broom"];
+    return(newCats);
+}
+
+function prependCat() {
+    let newCats = ["Arnold", ...cats];
+    return(newCats);
+}
+
+function removeLastCat() {
+    let newCats = [...cats];
+    return(newCats.slice(0,-1));
+}
+
+function removeFirstCat() {
+    let newCats = [...cats];
+    return(newCats.slice(1));
+}
